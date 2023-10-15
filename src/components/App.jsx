031f3669b9f4ab404cdef53ccca1ac8c642e623a
Max-IT-ZT <babuskomax@gmail.com>
2023-10-15 03:39:46 +0300

    first

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
+import { Profile } from './Profile/Profile';
+import { FriendList } from './FriendList/FriendList';
+import { Statistics } from './Statistics/Statistics';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <section className="section">
+      <div className="container">
+        <Profile />
+      </div>
+
+      <div className="container">
+        <Statistics />
+      </div>
+      <div className="container">
+        <FriendList />
+      </div>
+      <div className="container">
+        <TransactionHistory />
+      </div>
+    </section>
   );
 };
